718a2027807cb78d74cda274b6f8f79352c6544f
EvieePy <29671945+EvieePy@users.noreply.github.com>
2026-04-07 01:04:12 +1000

    Add sync command

--- extensions/twit/admin.py
+++ extensions/twit/admin.py
@@ -21,7 +21,11 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-import core
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    import core
 
 
 async def setup(bot: core.TwitchBot) -> None: ...
